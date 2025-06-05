29e14d35078178e949fcd95c93550b9707170023
Honoka Nishiyama <hono.nish6555@gmail.com>
2025-06-05 23:28:33 +0900

    Add source files

--- docs/answer-tracker.js
+++ docs/answer-tracker.js
@@ -12,7 +12,7 @@ document.addEventListener("click", function (event) {
     console.log("❌ .quiz が見つかりません");
     return;
   }
-  
+
   const question = questionEl?.querySelector(".title.content > p")?.innerText.trim() ?? "Unknown question";
   const answer = choice.innerText.trim();
 
@@ -24,7 +24,7 @@ document.addEventListener("click", function (event) {
 
   console.log("✅ 送信準備OK:", { question, answer, correct });
 
-  fetch("https://script.google.com/macros/s/AKfycbzbnLTbinjMT5rYM-YjNHyfoJ0Y2JG7UhfrBIxZ2bGPJFHSTfIZrw5_DEaUTcf4ejBW/exec", {
+  fetch("https://script.google.com/macros/s/AKfycbzZA3aGQjQvANHl2Hh7z98eUoRQf1VK6hcghsLiR2A75cDwtQp44_3NRiBRxpSeBAAq/exec", {
     method: "POST",
     body: JSON.stringify({ question, answer, correct }),
     headers: {
